fd358307b362b4dd9304df7d24108097ef22f445
mk-singhal <maniksingal29@gmail.com>
2022-08-04 23:11:35 +0530

    Added Auth and social auth

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -11,9 +11,22 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Reading .env files for Email credentials
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+# Email Reset Password link
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +38,7 @@ SECRET_KEY = 'django-insecure-g!xlyzxc&+#s9^*v6bajh&j)j@k+zfc3ebykmx9$564h4kiy(l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -112,7 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -126,7 +141,24 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# custom authentication backend
+# https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#other-authentication-sources
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+# Google keys
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
