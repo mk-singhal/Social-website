83583f8d15f1a985114280cfd04cbe69665af2c0
mk-singhal <maniksingal29@gmail.com>
2022-08-24 19:41:57 +0530

    Activity stream and image ranking

--- account/apps.py
+++ account/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+# from django.apps import 
 
 class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'account'
+    def ready(self):
+        # import signal handlers
+        import images.signals
